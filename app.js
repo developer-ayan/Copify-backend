ef263a74b82364779b0f56159092ddac1f98c587
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2024-09-19 01:03:12 +0500

    * global ip remove

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/app', appRoutes);
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
