5a69c8b2d21425b7b9b142f891bd1e10645a6d8f
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2024-09-21 22:54:26 +0500

    * undo file

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 const { database_name } = require('../utils/static-values');
 
-const username = 'ayanahmed255';
-const password = 'Hello786@';
+const username = 'ayanahmed255'; // Replace with your actual username
+const password = 'Hello786@'; // Replace with your actual password (URL-encoded if necessary)
 
-mongoose.connect(`mongodb+srv://${username}:${encodeURIComponent(password)}@cluster0.kffyovn.mongodb.net/${database_name}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(`mongodb+srv://${username}:${encodeURIComponent(password)}@cluster0.kffyovn.mongodb.net/${database_name}`)
   .then(() => {
     console.log("Connected to MongoDB");
   })
